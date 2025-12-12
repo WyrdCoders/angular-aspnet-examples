ac16ad18a8a7589c729c361d0765272280964d3e
Danny Glenfield <dglenfield@gmail.com>
2025-12-11 20:48:14 -0500

    Fix typo in app.component.spec.ts so the tests run

--- HealthCheck/healthcheck.client/src/app/app.component.ts
+++ HealthCheck/healthcheck.client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+// Interface to store the weather forecast data 
 interface WeatherForecast {
   date: string;
   temperatureC: number;
