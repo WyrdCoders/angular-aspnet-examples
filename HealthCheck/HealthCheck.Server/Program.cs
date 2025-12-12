0cee8936a5d31fb4290bc7d084f8e94427f6921a
Danny Glenfield <dglenfield@gmail.com>
2025-12-11 20:15:21 -0500

    Add comments in Program.cs and correct the port number in HealthCheck.Server.http

--- HealthCheck/HealthCheck.Server/Program.cs
+++ HealthCheck/HealthCheck.Server/Program.cs
@@ -1,30 +1,44 @@
+// Instantiate a WebApplicationBuilder
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Add services to the container
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#endregion
+
+// Create a WebApplication object which implements IHost
 var app = builder.Build();
+
+#region Configure the HTTP request pipeline by loading the required middleware and services
+
+// The middleware and services added to the HTTP pipeline will process incoming requests in registration order, from top to bottom.
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
+// Use Swagger in the development environment
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Handle HTTP to HTTPS redirects
 app.UseHttpsRedirection();
 
+// Allow requests to be restricted to authorized users
 app.UseAuthorization();
 
+// Add the endpoints required by the controller action methods to handle incoming HTTP requests with default routing rules
 app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
 
+#endregion
+
+// Run the app
 app.Run();
