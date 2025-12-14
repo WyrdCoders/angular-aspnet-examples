5cf2dd26b1135017c22e316942e64ea926a76ecd
Danny Glenfield <dglenfield@gmail.com>
2025-12-14 10:15:16 -0500

    Refactor the Angular app and add navigation - Add a top-level navigation menu. - Implement a client-side routing system. - Move the existing weather forecast example to a separate component.

--- HealthCheck/healthcheck.client/src/app/app.component.ts
+++ HealthCheck/healthcheck.client/src/app/app.component.ts
@@ -1,39 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-// Interface to store the weather forecast data 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/api/weatherforecast')
-      .subscribe({
-        next: (result) => {
-          this.forecasts = result;
-        },
-        error: (error) => {
-          console.error(error);
-        }
-      });
-  }
-
-  title = 'healthcheck.client';
+export class AppComponent {
+  title = 'HealthCheck';
 }
--- HealthCheck/healthcheck.client/src/app/fetch-data/fetch-data.component.ts
+++ HealthCheck/healthcheck.client/src/app/fetch-data/fetch-data.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html',
   styleUrl: './fetch-data.component.css'
 })
-export class FetchDataComponent {
+export class FetchDataComponent implements OnInit {
+  public forecasts?: WeatherForecast[];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getForecasts();
+  }
+
+  private getForecasts(): void {
+    this.http.get<WeatherForecast[]>(environment.baseUrl + 'api/weatherforecast')
+      .subscribe({
+        next: (result) => this.forecasts = result,
+        error: (error) => console.error(error)
+      });
+  }
+}
+
+// Interface to store the weather forecast data
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
