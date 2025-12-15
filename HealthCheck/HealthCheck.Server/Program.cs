bd25233515116e6574192b727dd7713e45dcb503
Danny Glenfield <dglenfield@gmail.com>
2025-12-15 09:58:33 -0500

    Add the HealthCheck middleware

--- HealthCheck/HealthCheck.Server/Program.cs
+++ HealthCheck/HealthCheck.Server/Program.cs
@@ -3,7 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 #region Add services to the container
 
+// Learn more about Health Checkes at https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks
+builder.Services.AddHealthChecks();
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -32,6 +36,11 @@ app.UseHttpsRedirection();
 
 // Allow requests to be restricted to authorized users
 app.UseAuthorization();
+
+// Configure Health Checks
+// Adding before MapControllers ensures the HealthChecks route won't be overridden
+// by any controller that might share that same route in the future.
+app.UseHealthChecks(new PathString("/api/health"));
 
 // Add the endpoints required by the controller action methods to handle incoming HTTP requests with default routing rules
 app.MapControllers();
