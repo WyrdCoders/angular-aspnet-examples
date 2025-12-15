9bc32c20e6a1a27988753242eb68a3b26d39104a
Danny Glenfield <dglenfield@gmail.com>
2025-12-15 11:08:30 -0500

    Add PingHealthCheck for use with the HealthCheck middleware - Host is hard-coded to simulate an unhealthy scenario for initial testing

---
+++ HealthCheck/HealthCheck.Server/PingHealthCheck.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.NetworkInformation;
+
+namespace HealthCheck.Server;
+
+/// <summary>
+/// Implements an Internet Control Message Protocol (ICMP) health check.
+/// </summary>
+public class PingHealthCheck : IHealthCheck
+{
+    // Set host to a non-routable IP address to simulate an "unhealthy" scenario
+    private readonly string _host = "10.0.0.0";
+    private readonly int _healthyRoundTripTime = 300;
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+		try
+		{
+			using var ping = new Ping();
+			var reply = await ping.SendPingAsync(_host);
+            return reply.Status switch
+            {
+                IPStatus.Success => (reply.RoundtripTime > _healthyRoundTripTime) 
+                    ? HealthCheckResult.Degraded() : HealthCheckResult.Healthy(),
+                _ => HealthCheckResult.Unhealthy(),
+            };
+        }
+		catch (Exception)
+		{
+			return HealthCheckResult.Unhealthy();
+		}
+    }
+}
--- HealthCheck/HealthCheck.Server/Program.cs
+++ HealthCheck/HealthCheck.Server/Program.cs
@@ -1,25 +1,19 @@
+using HealthCheck.Server;
+
 // Instantiate a WebApplicationBuilder
 var builder = WebApplication.CreateBuilder(args);
 
-#region Add services to the container
+/* Add services to the container */
 
-// Learn more about Health Checkes at https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks
-builder.Services.AddHealthChecks();
-
+builder.Services.AddHealthChecks().AddCheck<PingHealthCheck>("Ping");
 builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-#endregion
 
 // Create a WebApplication object which implements IHost
 var app = builder.Build();
 
-#region Configure the HTTP request pipeline by loading the required middleware and services
-
-// The middleware and services added to the HTTP pipeline will process incoming requests in registration order, from top to bottom.
+/* Configure the HTTP request pipeline by loading the required middleware and services */
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
@@ -37,17 +31,16 @@ app.UseHttpsRedirection();
 // Allow requests to be restricted to authorized users
 app.UseAuthorization();
 
-// Configure Health Checks
-// Adding before MapControllers ensures the HealthChecks route won't be overridden
-// by any controller that might share that same route in the future.
+// Configure HealthChecks
+/* Adding before MapControllers ensures the HealthChecks route won't be overridden
+ * by any controller that might share that same route in the future.
+ * Learn more about Health Checkes at https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks */
 app.UseHealthChecks(new PathString("/api/health"));
 
 // Add the endpoints required by the controller action methods to handle incoming HTTP requests with default routing rules
 app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
-
-#endregion
 
 // Run the app
 app.Run();
