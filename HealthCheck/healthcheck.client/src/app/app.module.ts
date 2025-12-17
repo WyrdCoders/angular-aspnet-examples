95aaf26c94b34580db4d901c8a9fa7221bd8b690
Danny Glenfield <dglenfield@gmail.com>
2025-12-17 09:27:14 -0500

    Add the HealthCheckComponent. Update the nav menu and routing to allow access to the component.

--- HealthCheck/healthcheck.client/src/app/app.module.ts
+++ HealthCheck/healthcheck.client/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FetchDataComponent,
     HomeComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    HealthCheckComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
