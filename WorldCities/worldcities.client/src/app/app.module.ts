05cdb6c29a79faefb6ec4abaf5d214a986dabbc6
Danny Glenfield <dglenfield@gmail.com>
2025-12-18 12:12:40 -0500

    Update the Angular app with a home page and nav bar
    - Add HomeComponent and NavMenuComponent.
    - Install Angular Material.
    - Add MatToolbar to the NavMenuComponent.
    - Remove the template example code for weather forecasts.

--- WorldCities/worldcities.client/src/app/app.module.ts
+++ WorldCities/worldcities.client/src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+// Angular Material Modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
