67ae7417ad70a761557c4088cf3ae05254cbac32
Danny Glenfield <dglenfield@gmail.com>
2025-12-18 11:35:40 -0500

    Create the WorldCities projects using the Angular and ASP.NET Core template
    - Add worldcities.client
    - Add WorldCities.Server
    - Generate the Angular environments folder and files

---
+++ WorldCities/worldcities.client/src/proxy.conf.js
@@ -0,0 +1,15 @@
+const { env } = require('process');
+
+const target = 'https://localhost:40443';
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/weatherforecast",
+    ],
+    target,
+    secure: false
+  }
+]
+
+module.exports = PROXY_CONFIG;
