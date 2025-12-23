4eefe7d2a14c8c0ea8b78ea664536e77186f020a
Danny Glenfield <dglenfield@gmail.com>
2025-12-23 09:29:54 -0500

    Define the database table names for City and Country entities

--- WorldCities/WorldCities.Server/Data/Models/Country.cs
+++ WorldCities/WorldCities.Server/Data/Models/Country.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WorldCities.Server.Data.Models;
 
+[Table("Countries")]
 public class Country
 {
     /// <summary>
