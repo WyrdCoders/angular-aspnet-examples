aa7dd9c70ae1e37513bcedf5a51aaa6952da43f3
Danny Glenfield <dglenfield@gmail.com>
2025-12-24 10:10:13 -0500

    Revert back to using data annotations for EF Core configuration
    - Remove Fluent API example code

--- WorldCities/WorldCities.Server/Data/ApplicationDbContext.cs
+++ WorldCities/WorldCities.Server/Data/ApplicationDbContext.cs
@@ -15,16 +15,4 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<City> Cities => Set<City>();
     public DbSet<Country> Countries => Set<Country>();
-
-    /* Fluent API with EntityTypeConfiguration classes Example
-     * Fluent API configurations override any existing EF Core convention or data annotations
-     * applied to entity classes and properties.
-     */
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        // Add the EntityTypeConfiguration classes
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
-    }
 }
